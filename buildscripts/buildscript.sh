2f7fa531703523d85d9d1e219a0955220f44bbaa
Witold <wewolski@gmail.com>
2014-05-07 17:37:08 +0200

    [B] sudo make test

--- buildscripts/buildscript.sh
+++ buildscripts/buildscript.sh
@@ -2,6 +2,6 @@ sudo mkdir findMFHCSbuild
 cd findMFHCSbuild
 sudo cmake -DFindMFBase_INCLUDE_DIR=/home/travis/build/findMF/findMFHCS/findMFBase/include -DPwiz_INCLUDE_DIR=/home/travis/build/findMF/findMFHCS/pwiz/pwiz -DPwiz_LIBRARY=/home/travis/build/findMF/findMFHCS/pwiz/lib ..
 sudo make 
-make test
+sudo make test
 
 
