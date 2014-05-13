fc6b0efd0dc73f101809352a8b54c2bdbaf800fa
Witold <wewolski@gmail.com>
2014-05-13 14:02:32 +0200

    [BUILD] tmp

--- buildscripts/buildVigra.sh
+++ buildscripts/buildVigra.sh
@@ -1,7 +1,11 @@
 sudo git clone https://github.com/ukoethe/vigra.git
 vigrabuild=$(pwd)/vigrabuild #place products here
+
+echo $vigrabuild
+
 sudo mkdir vigra-build
 cd vigra-build
+
 sudo cmake -DCMAKE_INSTALL_PREFIX=$vigrabuild -DWITH_HDF5=OFF -DWITH_VIGRANUMPY=OFF ../vigra
 sudo make
 sudo make install
--- buildscripts/buildscript.sh
+++ buildscripts/buildscript.sh
@@ -2,7 +2,8 @@ workdir=$(pwd)
 sudo mkdir findMFHCSbuild
 cd findMFHCSbuild
 
-sudo cmake -DFindMFBase_INCLUDE_DIR=$workdir/findMFBase/include -DPwiz_INCLUDE_DIR=$workdir/pwiz/pwiz -DPwiz_LIBRARY=$workdir/pwiz/lib -DVIGRA_INCLUDE_DIR=$workdir/vigrabuild/lib -DVIGRA_INCLUDE_DIR=$workdir/vigrabuild/include/ -DFindMFInterfaces_INCLUDE_DIR=$workdir/findMFInterfaces/include ..
-sudo make 
+sudo cmake -DFindMFBase_INCLUDE_DIR=$workdir/findMFBase/include -DPwiz_INCLUDE_DIR=$workdir/pwiz/pwiz -DPwiz_LIBRARY=$workdir/pwiz/lib -DVIGRA_LIBRARY=$workdir/vigrabuild/lib -DVIGRA_INCLUDE_DIR=$workdir/vigrabuild/include/ -DFindMFInterfaces_INCLUDE_DIR=$workdir/findMFInterfaces/include ..
+
+sudo make
 sudo make test
 
