eaf858c13048182738be0e32ce382c1897347967
Witold Wolski <wewolski@gmail.com>
2014-06-09 15:04:19 +0200

    Update filterSingleMap.sh
    [FIX] adding sudo

--- scripts/test/filterSingleMap.sh
+++ scripts/test/filterSingleMap.sh
@@ -2,11 +2,11 @@
 PROJECTDIR=$(pwd)
 sudo mkdir $PROJECTDIR/tmp1
 ####### gaussian smoothing with mzscale=1.3 and background removal with structuring element 35 in RT and 25 in mz #######
-$PROJECTDIR/findMFHCSout/filterSingleMap --mzscale=1.3 --resolution=35000 --minMass=200 --nrthreads=2 --width-RT=35 --width-MZ=25 --rtscale=1.3 --in=$PROJECTDIR/submodule/findMFData/dil_6_2.mzML --outdir=$PROJECTDIR/tmp
+sudo $PROJECTDIR/findMFHCSout/filterSingleMap --mzscale=1.3 --resolution=35000 --minMass=200 --nrthreads=2 --width-RT=35 --width-MZ=25 --rtscale=1.3 --in=$PROJECTDIR/submodule/findMFData/dil_6_2.mzML --outdir=$PROJECTDIR/tmp
 ##### setting -width-RT=0 will disable background removal.
-mkdir $PROJECTDIR/tmp2
-$PROJECTDIR/findMFHCSout/filterSingleMap --mzscale=1.3 --resolution=35000 --minMass=200 --nrthreads=2 --width-RT=0 --width-MZ=0 --rtscale=1.3 --in=$PROJECTDIR/submodule/findMFData/dil_6_2.mzML --outdir=$PROJECTDIR/tmp2
+sudo mkdir $PROJECTDIR/tmp2
+sudo $PROJECTDIR/findMFHCSout/filterSingleMap --mzscale=1.3 --resolution=35000 --minMass=200 --nrthreads=2 --width-RT=0 --width-MZ=0 --rtscale=1.3 --in=$PROJECTDIR/submodule/findMFData/dil_6_2.mzML --outdir=$PROJECTDIR/tmp2
 ##### disable filtering with enable=0
-mkdir $PROJECTDIR/tmp3
+sudo mkdir $PROJECTDIR/tmp3
 $PROJECTDIR/findMFHCSout/filterSingleMap --mzscale=1.3 --resolution=35000 --minMass=200 --nrthreads=2 --enable=0 --in=$PROJECTDIR/submodule/findMFData/dil_6_2.mzML --outdir=$PROJECTDIR/tmp3
 
