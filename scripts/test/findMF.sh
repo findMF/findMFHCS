2d346e1ae652c485e5e76175b598a5a1f8f15bc0
Witold Wolski <wewolski@gmail.com>
2014-06-09 15:04:52 +0200

    Update findMF.sh
    [FIX] adding sudo

--- scripts/test/findMF.sh
+++ scripts/test/findMF.sh
@@ -5,7 +5,7 @@ sudo mkdir $PROJECTDIR/tmp
 
 ####### Feature finding #######
 # example with feature detection in file with single map. Output is an dil_6_2.sqlite file
-$PROJECTDIR/findMFHCSout/findMF --mzscale=1.3 --resolution=65000 --minMass=200 --nrthreads=2 --minintensity=5. --in=$PROJECTDIR/submodule/findMFData/dil_6_2.mzML --outdir=$PROJECTDIR/tmp
+sudo $PROJECTDIR/findMFHCSout/findMF --mzscale=1.3 --resolution=65000 --minMass=200 --nrthreads=2 --minintensity=5. --in=$PROJECTDIR/submodule/findMFData/dil_6_2.mzML --outdir=$PROJECTDIR/tmp
 # example of feature detection in file with many maps. Output is a file named testS.sqlite
-$PROJECTDIR/findMFHCSout/findMF --mzscale=1.3 --resolution=65000 --minMass=200 --nrthreads=2 --minintensity=5. --in=$PROJECTDIR/submodule/findMFData/testS.mzML --outdir=$PROJECTDIR/tmp
+sudo $PROJECTDIR/findMFHCSout/findMF --mzscale=1.3 --resolution=65000 --minMass=200 --nrthreads=2 --minintensity=5. --in=$PROJECTDIR/submodule/findMFData/testS.mzML --outdir=$PROJECTDIR/tmp
 
