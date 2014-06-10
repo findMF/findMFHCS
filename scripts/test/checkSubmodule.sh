e295ed57a5bef23c4062f159f5958c8535446ccd
Witold <wewolski@gmail.com>
2014-06-10 13:15:09 +0200

    [FIX]

--- scripts/test/checkSubmodule.sh
+++ scripts/test/checkSubmodule.sh
@@ -2,4 +2,4 @@ export PROJECTDIR=$(pwd)
 
 # prepare the data file
 sudo unzip $PROJECTDIR/submodule/findMFData/dil_6_2.mzML.zip -d $PROJECTDIR/submodule/findMFData/
-sudo $PROJECTDIR/scritps/test/pp2.sh
+sudo $PROJECTDIR/scripts/test/pp2.sh
