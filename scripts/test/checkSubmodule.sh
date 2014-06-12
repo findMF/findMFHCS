ff4ce7730bdf2ecb6319c734c72687bb5a7154f4
Witold <wewolski@gmail.com>
2014-06-12 17:50:29 +0200

    [FIX/TEST]

--- scripts/test/checkSubmodule.sh
+++ scripts/test/checkSubmodule.sh
@@ -3,3 +3,5 @@ export PROJECTDIR=$(pwd)
 # prepare the data file
 sudo unzip $PROJECTDIR/submodule/findMFData/dil_6_2.mzML.zip -d $PROJECTDIR/submodule/findMFData/
 sudo $PROJECTDIR/scripts/test/pp2.sh
+sudo $PROJECTDIR/scripts/test/filterSingleMap.sh
+sudo $PROJECTDIR/scripts/test/findMF.sh
