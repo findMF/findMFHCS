191c580602315c2221d0581ac2f8f427778e3e99
Witold <wewolski@gmail.com>
2014-06-13 11:43:43 +0200

    [FIX] remove test to limit runtime

--- scripts/test/checkSubmodule.sh
+++ scripts/test/checkSubmodule.sh
@@ -4,4 +4,4 @@ export PROJECTDIR=$(pwd)
 sudo unzip $PROJECTDIR/submodule/findMFData/dil_6_2.mzML.zip -d $PROJECTDIR/submodule/findMFData/
 sudo $PROJECTDIR/scripts/test/pp2.sh
 sudo $PROJECTDIR/scripts/test/filterSingleMap.sh
-sudo $PROJECTDIR/scripts/test/findMF.sh
+#sudo $PROJECTDIR/scripts/test/findMF.sh
